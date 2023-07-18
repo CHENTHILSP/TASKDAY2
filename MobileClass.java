79e0fb60e8f2f37c63abf7701eb4521adbeeb417
CHENTHILSP <132836556+CHENTHILSP@users.noreply.github.com>
2023-07-18 21:39:27 +0530

    Add files via upload

---
+++ MobileClass.java
@@ -0,0 +1,25 @@
+package julyday2;
+
+public class MobileClass {
+
+	public static void main(String[] args) {
+		
+		String mobileBrandName="VERTU";
+		System.out.println(mobileBrandName);
+		char mobileLogo='V';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=5;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=18;
+		System.out.println(modelNumber);
+		long mobilelmeiNumber=234678593789L;
+		System.out.println( mobilelmeiNumber);
+		float mobilePrice=750000f;
+		System.out.println(mobilePrice);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+		
+
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,22 @@
+package julyday2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrons = 2;
+		System.out.println(noOfMirrons);
+		long chassisNumber = 384756839028L;
+		System.out.println(chassisNumber);
+		boolean isPunctured = false;
+		System.out.println(isPunctured);
+		String bikeName = "AUDI";
+		System.out.println(bikeName);
+		double runningKM = 1800.0;
+		System.out.println(runningKM);
+
+	}
+
+}
